f2afe3a3989eae5ec14695924fdd3cc5265ce22a
Ross Friscia <rossfriscia14@gmail.com>
2022-03-10 11:53:28 -0800

    Add setup.py to repository.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sports-book-manager',
+    version='1.0.0',
+    author='Ross Friscia',
+    author_email='rafrisci@uw.edu',
+    description='Get the odds in your favor',
+    url='https://github.com/rafrisci/sports-book-manager',
+    packages = find_packages(),
+    install_requires=[
+        'selenium',
+        'scipy',
+        'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
