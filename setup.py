ad14e31c467284ba3aa60a033099f6329feb03fd
Ross Friscia <rossfriscia14@gmail.com>
2022-03-11 13:24:19 -0800

    Get package to recognize the csv files in the data folder.

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     description='Get the odds in your favor',
     url='https://github.com/rafrisci/sports-book-manager',
     packages = find_packages(),
+    package_data={
+        'sports-book-manager': ['data/*.csv']
+    },
     install_requires=[
         'selenium',
         'scipy',
