4f37a6f662306d7d60b3afaf967486d26b5bb5af
Ross Friscia <rossfriscia14@gmail.com>
2022-03-10 16:29:35 -0800

    Updated the docstrings for both the implied and model probability calculators.

--- sports_book_manager/implied_probability_calculator.py
+++ sports_book_manager/implied_probability_calculator.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
-Converts the line in the data into probability the outcome occurs.
-
 This function assumes that the line are presented in American line. This means
 that 'favorites' are given line -100 or lower while 'underdogs' are given line
 100 or greater.
 """
 
-def implied_probability_calculator(line):
+def implied_probability_calculator(odds):
+    """
+    Converts odds in the data into probability the outcome occurs.
+
+    Parameters:
+        odds: the weighted return on the wager made if correct
+    """
+
     if abs(int(line)) < 100:
         raise ValueError('American lines must be three digits or longer.')
     if line < 0:
--- sports_book_manager/model_probability.py
+++ sports_book_manager/model_probability.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """
-Get the probability an event occurs at the margin set with the market line.
-
-the model mean is added to the line because the model being positive means the
+The model mean is added to the line because the model being positive means the
 model expects the team to be better. However, the line being positive means the
 market expects the team to be worse. Since they are calculated differently,
 the line needs to change signs to match the model output.
@@ -10,5 +8,13 @@ the line needs to change signs to match the model output.
 from scipy.stats import norm
 
 def model_probability(model_mean, model_sd, line):
+    """
+    Get the probability an event occurs at the margin set with the market line.
+
+    Parameters:
+        model_mean: the expected margin of victory from the model
+        model_sd: the standard deviation of the model's margin of victory
+        line: the margin of victory the market is setting the wager at
+    """
     z_score = (model_mean + line)/model_sd
     return norm.cdf(z_score)
