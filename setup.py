015d84f523035aee8995833a4d877fb01296a3b5
Ross Friscia <rossfriscia14@gmail.com>
2022-03-11 22:29:30 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 from setuptools import setup, find_packages
 
 def _find_packages():
     packages = find_packages()
-    packages.append('data')
+    packages.append('sports-book-manager.data')
     return packages
 
 
@@ -14,6 +15,10 @@ setup(
     description='Get the odds in your favor',
     url='https://github.com/rafrisci/sports-book-manager',
     packages = _find_packages(),
+    package_dir={
+        'sports-book-manager.data': 'data',
+        'sports_book_manager': 'sports_book_manager'
+    },
     include_package_data=True,
     install_requires=[
         'selenium',
