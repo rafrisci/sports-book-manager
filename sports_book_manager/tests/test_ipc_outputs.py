428b2caa41a39a2696b6db8e098fe2af1495ce69
Ross Friscia <rossfriscia14@gmail.com>
2022-03-13 18:53:52 -0700

    fix implied_probability_calculator test formatting

--- sports_book_manager/tests/test_ipc_outputs.py
+++ sports_book_manager/tests/test_ipc_outputs.py
@@ -13,11 +13,18 @@ class TestIPC(unittest.TestCase):
         the ValueError is raised.
         """
         self.assertRaises(ValueError, ipc.implied_probability_calculator,
-                          ['help', 40, 15/2])
+                          'help')
+        self.assertRaises(ValueError, ipc.implied_probability_calculator,
+                          15/2)
+        self.assertRaises(ValueError, ipc.implied_probability_calculator,
+                          40)
 
     def test_valid_tests(self):
         """
         Tests valid inputs for the implied_probability_calculator.
         """
-        self.assertTrue(ipc.implied_probability_calculator([-110, 120, -150,
-                                                            100, 210]))
+        self.assertTrue(ipc.implied_probability_calculator(-150))
+        self.assertTrue(ipc.implied_probability_calculator(-110))
+        self.assertTrue(ipc.implied_probability_calculator(120))
+        self.assertTrue(ipc.implied_probability_calculator(100))
+        self.assertTrue(ipc.implied_probability_calculator(210))
